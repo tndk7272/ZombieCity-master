2797a8c207a1798216c39279c9ab28365579e6c0
tndk7272 <82929334+tndk7272@users.noreply.github.com>
2021-08-10 17:00:49 +0900

    LookAtCamere를 컴포넌트에 붙이면 LookAtConstraint 가 알아서 붙도록 하는 코드 구현

--- Assets/LookAtCamere.cs
+++ Assets/LookAtCamere.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Animations;
 
+
+
+
+[RequireComponent(typeof(LookAtConstraint))]
 public class LookAtCamere : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
+        var lookAtConstraint = GetComponent<LookAtConstraint>();
+        var source = new ConstraintSource();
+        source.sourceTransform = Camera.main.transform;
+        source.weight = 1;
         
-    }
+        lookAtConstraint.AddSource(source);
+        lookAtConstraint.constraintActive = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+
+
     }
 }
